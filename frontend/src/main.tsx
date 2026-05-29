a7eeea7b6c7b8dba76e21030921803b275526a3a
Muhammad-Husnain07 <muhammad.husnain.dev@gmail.com>
2026-05-29 17:19:07 +0500

    fix: import ThemeProvider/createTheme from @mui/material instead of @mui/material/styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ThemeProvider } from '@mui/material/styles'
+import { ThemeProvider } from '@mui/material'
 import CssBaseline from '@mui/material/CssBaseline'
 import './index.css'
 import App from './App.tsx'
